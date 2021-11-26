d6c8870023e10fef65c3ce7bc790750cf1f97f85
samchong <cjsam6@yahoo.com>
2021-11-26 12:03:06 +0800

    🚧 Discount card

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_food_menu_app/components/search_box.dart';
 import 'package:flutter_food_menu_app/constants.dart';
 import 'package:flutter_food_menu_app/screens/home/components/category_list.dart';
+import 'package:flutter_food_menu_app/screens/home/components/discount_card.dart';
 import 'package:flutter_food_menu_app/screens/home/components/item_list.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Body extends StatelessWidget {
 	const Body({ Key? key }) : super(key: key);
@@ -10,10 +12,12 @@ class Body extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 		return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
 			children: <Widget>[
 				SearchBox(onChanged: (value) {},),
 				CategoryList(),
 				ItemList(),
+                DiscountCard(),
 			],
 		);
 	}
